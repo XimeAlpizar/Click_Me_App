59c9808da05eb5f89f7adb4be08625cf8d086635
XimeAlpizar <0225683@up.edu.mx>
2022-02-17 14:32:40 -0600

    Se agrega el proyecto

---
+++ app/src/main/java/com/example/holamundo/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.example.holamundo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+
+private val TAG = "MainActivity"
+private val TEXT = "TEXT_CONTENT"
+class MainActivity : AppCompatActivity() {
+
+    private var textView: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate")
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.click_me_app)
+
+        val userInput: EditText = findViewById<EditText>(R.id.editText)
+        val button: Button = findViewById<Button>(R.id.button4)
+        textView = findViewById<TextView>(R.id.textView2)
+
+        textView?.text =""
+        textView?.movementMethod = ScrollingMovementMethod()
+
+
+        button.setOnClickListener(object :View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Log.d(TAG, "onClick")
+                textView?.append(userInput.text)
+                textView?.append("\n")
+                // userInput.setText("")
+                userInput.text.clear()
+            }
+        })
+    }
+
+    override fun onStart(){
+        Log.d(TAG, "onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d(TAG, "onResume")
+        super.onResume()
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        Log.d(TAG, "onRestoreInstanceState")
+        super.onRestoreInstanceState(savedInstanceState)
+        textView?.text = savedInstanceState.getString(TEXT, "")
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "onPause")
+        super.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.d(TAG, "onSaveInstanceState")
+        super.onSaveInstanceState(outState)
+        outState.putString(TEXT, textView?.text.toString())
+    }
+
+    override fun onStop() {
+        Log.d(TAG, "onStop")
+        super.onStop()
+    }
+
+    override fun onRestart() {
+        Log.d(TAG, "onRestart")
+        super.onRestart()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy")
+        super.onDestroy()
+    }
+
+
+}
